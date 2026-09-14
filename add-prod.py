5d32a3538bb8e9394138f57bd35696e39396f2ef
baremetal245 <aditya24g@protonmail.com>
2026-09-14 16:35:56 +0530

    feat: adding new brand to prod catalogue

---
+++ add-prod.py
@@ -0,0 +1,32 @@
+# Sample catalog structure
+catalog = {
+    "categories": {
+        "detergent category": {
+            "name": "Detergent Category",
+            "products": []
+        }
+    }
+}
+
+# Product data to add
+new_product = {
+    "id": "PROD_GD_001",
+    "name": "Godrej Liquid Detergent",
+    "brand": "Godrej",
+    "price": 180.00,
+    "unit": "1L",
+    "in_stock": True
+}
+
+# Logic to add the product safely
+def add_product_to_category(catalog_data, category_key, product):
+    categories = catalog_data.get("categories", {})
+    
+    if category_key in categories:
+        categories[category_key]["products"].append(product)
+        print(f"Successfully added '{product['name']}' to '{category_key}'.")
+    else:
+        print(f"Error: Category '{category_key}' does not exist.")
+
+# Execute
+add_product_to_category(catalog, "detergent category", new_product)
